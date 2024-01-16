39d0c453850cbf6c660f4589c115c7ad6770d960
PabloSczn <sancheznarro.pablo@gmail.com>
2024-01-16 22:55:54 +0000

    Added form to create a Camp

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.send('home')
 })
@@ -25,11 +27,21 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index.ejs', {campgrounds})
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new.ejs');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const id = req.params.id;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show.ejs', {campground})
-})
+});
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000');
