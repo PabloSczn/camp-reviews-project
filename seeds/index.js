0421639a2989300a34b4a33b233d5eb52fafe349
PabloSczn <sancheznarro.pablo@gmail.com>
2024-02-06 23:44:02 +0000

    Added image and description

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDb = async()=> {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt necessitatibus accusantium fugit temporibus neque, quia ipsam reprehenderit sed nobis iusto laudantium quod velit eum illum nostrum voluptates dicta libero! Exercitationem.',
+            price
         });
         await camp.save();
     }
